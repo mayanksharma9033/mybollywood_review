99b77c49f373b7e4e868bb42777c30d6c16cd1c2
mayank sharma <mayanksharma9033@gmail.com>
2017-05-22 15:10:00 +0530

    reinitilaized files

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,52 @@
+class MoviesController < ApplicationController
+ 
+ def index
+  @movies=Movie.all
+ end
+
+ def show
+  @movie=Movie.find(params[:id])
+ end
+
+ def new
+   @movie=Movie.new
+ end
+
+def create
+	@movie=Movie.new(movie_params)
+
+    if @movie.save
+    redirect_to movies_path
+    else
+   	render 'new'
+  end
+end
+
+def edit
+@movie=Movie.find(params[:id])
+end
+
+def update
+@movie=Movie.find(params[:id])
+
+if @movie.update(movie_params)
+
+  redirect_to movie_path(@movie)
+  else
+    render :edit
+  end
+end
+
+def destroy
+@movie.destroy
+
+redirect_to movies_path
+end
+
+private
+
+def movie_params
+	params.require(:movie).permit(:movie_name,:description,:movie_length,:director_name,:rating)
+end
+
+end
